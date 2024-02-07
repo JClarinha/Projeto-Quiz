0206573f29ec0d874e6447323f44b3e127007ee4
JClarinha <joaoclarinha13@gmail.com>
2024-02-07 01:21:38 +0000

    Alteration from if else statement to switch case

--- Program.cs
+++ Program.cs
@@ -20,51 +20,81 @@ System.Console.WriteLine("Para que serve a programação?\n A- Para o pessoal po
 string Resposta1 = System.Console.ReadLine(); 
 int p1 = 10; // pontuação da pergunta
 int percentagem1 = p1*10/100;
-//percentagem1 = - Math.Abs(percentagem1); // transformação de numero inteiro em negativo 
 
 
-if (Resposta1 == "b" || Resposta1 =="B")
+
+switch(Resposta1)
 {
-    System.Console.WriteLine("Fantástico acabas de ganhar 10 pontos!");
-    pontos = pontos + p1;
-    System.Console.WriteLine("Pontos = " + pontos);
-}
+    case "b":
+    case "B":
+        System.Console.WriteLine("Fantástico acabas de ganhar 10 pontos!");
+        pontos = pontos + p1;
+        System.Console.WriteLine("Pontos = " + pontos);
+        break;
 
-else if (Resposta1 == "a" || Resposta1 == "A")
-{
-    System.Console.WriteLine("De facto muito conveniente mas acabas de perder 10% do valor total da pargunta :(");
-    pontos = pontos - percentagem1;
-    System.Console.WriteLine("Pontos = " + pontos);
-}
+    case "a":
+    case "A":
+        System.Console.WriteLine("De facto muito conveniente mas acabas de perder 10% do valor total da pargunta :(");
+        pontos = pontos - percentagem1;
+        System.Console.WriteLine("Pontos = " + pontos);
+        break;
 
-else if (Resposta1 == "c" || Resposta1 == "C")
-{
-    System.Console.WriteLine("Not an option my friend! Menos 10% do valor da pergunta e estou a ser amigo!");
-    pontos = pontos - percentagem1;
-    System.Console.WriteLine("Pontos = " + pontos);
-}
+    case "c":
+    case "C":
+        System.Console.WriteLine("Not an option my friend! Menos 10% do valor da pergunta e estou a ser amigo!");
+        pontos = pontos - percentagem1;
+        System.Console.WriteLine("Pontos = " + pontos);
+        break;
 
-else
-{
-    System.Console.WriteLine("Mas estás a brincar com isto? Toma lá menos 20 pontos");
-    pontos = pontos -20;
-    System.Console.WriteLine("Pontos = " + pontos);
-}
-System.Console.WriteLine("PILKA " + pontos);
+    default:
+        System.Console.WriteLine("Mas estás a brincar com isto? Toma lá menos 20 pontos");
+        pontos = pontos - 20;
+        System.Console.WriteLine("Pontos = " + pontos);
+        break;
 
-System.Console.WriteLine("Pergunta 2 = 20 pontos!\nHTML é uma linguagem de progrmação?\n A-Sim!\n B- Não!");
+}
+    
+
+
+System.Console.WriteLine("Pergunta 2 = 20 pontos!\nHTML é uma linguagem de progrmação?\n A- Sim!\n B- Não!");
 int p2 = 20; //pontos da pergunta
 string Resposta2 = System.Console.ReadLine();
 
-if (Resposta2 != "a" || Resposta2 != "A");
+
+switch(Resposta2)
 {
-    System.Console.WriteLine("Parabéns mais 20 pontos!");
-    System.Console.WriteLine("Pontos = " + pontos);
-    pontos = pontos + p2;
+    case "b":
+    case "B":
+        System.Console.WriteLine("Parabéns mais 20 pontos!");
+        pontos = pontos + p2;
+        System.Console.WriteLine("Pontos = " + pontos);
+       
+       break;
+
+     case "a":
+     case "A":
+        System.Console.WriteLine("Esta era fácil porfavor!");
+        int percentagem2 = p2 * 10 / 100;
+        pontos = pontos - percentagem2;
+        System.Console.WriteLine("Toma lá menos 10% do valor da resposta certa!");
+        System.Console.WriteLine("Pontos = " + pontos);
+        break;
+
+    default:
+        System.Console.WriteLine("No minimo tenta!");
+
+        pontos = pontos - (p2 * 2);
+        System.Console.WriteLine("Toma lá menos 40 pontos e é para os receberes com um sorriso!");
+        System.Console.WriteLine("Pontos = " + pontos);
+        break;
 }
+
+
+
 stopwatch.Stop();
 TimeSpan tp = stopwatch.Elapsed;
 string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", tp.Hours, tp.Minutes, tp.Seconds);
 System.Console.WriteLine("Tempo: " + elapsedTime);
 
-// Alterar if da ultima pergunta
+
+Console.ReadLine();
